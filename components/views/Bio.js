56dbadfea112c0d9256336c79a761a33602fd629
kwilkes314 <kendallwilkes.kw@gmail.com>
2023-04-26 21:21:16 -0500

    savvy spa

--- components/views/Bio.js
+++ components/views/Bio.js
@@ -1,6 +1,6 @@
 import html from "html-literal";
 
-export default () => html`
+export default (state) => html`
 
 
 <section id="bio">
---
+++ store/Links.js
@@ -0,0 +1,8 @@
+export default [
+  { title: "Home", text: "Home" },
+  { title: "Bio", text: "Bio" },
+  { title: "Order", text: "Submit a Pizza Order" },
+  { title: "Pizza", text: "See all the Pizzas" },
+];
+
+
